fa8579425679605fb232be7ba803a72f58c1fb68
Ruben Albertassi <albertassi88@gmail.com>
2026-03-10 11:56:14 -0300

    desafio 5 resolvido

---
+++ desafio5.py
@@ -0,0 +1,7 @@
+# Quando o carro e o caminhão se cruzam, eles estão no mesmo ponto da estrada
+
+# Os dois estão à mesma distância de Ribeirão Preto
+
+# Os pedágios só fazem o carro atrasar 15 minutos, mas não mudam o lugar onde eles se encontram, só o tempo do encontro
+
+# Resposta: Os dois estão à mesma distância de Ribeirão Preto
